f69f50502fb811b2ad832d21310bd77cbd75c8d0
java_DSA <jayprakashmcode7@gmail.com>
2023-10-25 01:30:16 +0530

    Adding the file of the code that prints fabonacci series

---
+++ generate_Fibonacci_series.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class generate_Fibonacci_series {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner (System.in);
+        int n=sc.nextInt();
+        int N1=0;
+        int N2=1;
+        if(n==1){
+            System.out.println(0);
+        }else if(n==2){
+            System.out.println(0);
+            System.out.println(1);
+        }else{
+            System.out.println(0);
+            System.out.println(1);
+            for(int i=3;i<=n;i++){
+                int fab=N1+N2;
+                System.out.println(fab);
+                N1=N2;
+                N2=fab;
+            }
+        }
+        sc.close();
+    }
+}
