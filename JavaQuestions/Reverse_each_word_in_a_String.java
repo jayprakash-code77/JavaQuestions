7f02fd84615769a58c8adbb71ac37b43e47fe6bb
java_DSA <jayprakashmcode7@gmail.com>
2023-11-17 01:34:52 +0530

    Modified the file

--- JavaQuestions/Reverse_each_word_in_a_String.java
+++ JavaQuestions/Reverse_each_word_in_a_String.java
@@ -2,22 +2,22 @@ package JavaQuestions;
 
 public class Reverse_each_word_in_a_String {
     public static void main(String[] args) {
-        String str="Welcome to Java";
+        String str="Welcome to Java";  // Original String
 
-        String[] words=str.split(" ");
+        String[] words=str.split(" ");  // Spliting the String into words
 
-        String reverseString="";
+        String reverseString="";  // Answer storing String
 
         for(String w:words){
 
             String reverseWord="";
-            for(int i=w.length()-1;i>=0;i--){
+            for(int i=w.length()-1;i>=0;i--){   // this will reverse each word
                 reverseWord=reverseWord+w.charAt(i);
             }
 
-            reverseString=reverseString+reverseWord+" ";
+            reverseString=reverseString+reverseWord+" "; // this will add all reversed words in the "recerseString";
         }
 
-        System.out.println(reverseString);
+        System.out.println(reverseString);   // this will print the reversed string.
     }
 }
