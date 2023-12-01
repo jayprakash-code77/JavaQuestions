1cc0c389c32bf9cb0f1f5edd01625799d747e4b8
java_DSA <jayprakashmcode7@gmail.com>
2023-12-01 23:55:14 +0530

    Adding file of code that calculates the power of the given number

---
+++ Recursion/Calculate_Power.java
@@ -0,0 +1,18 @@
+package Recursion;
+
+import javax.naming.spi.DirStateFactory.Result;
+
+public class Calculate_Power {
+
+
+public static int Pow(int Base, int Power,int Result){
+    if(Power==1){
+        return Result;
+    }
+    return Base * Pow(Base, Power-1,Result*Base);
+}
+    public static void main(String[] args) {
+       int ans= Pow(3, 2,1);
+       System.out.println(ans);
+    }
+}
