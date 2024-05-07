d828059ba399afbdfe0de3762009a4a3afee2ffb
Jaypraksh Maurya <jayprakashmcode7@gmail.com>
2024-05-07 00:11:11 -0700

    after reset

--- JavaQuestions/Binary_Search_algorithm.java
+++ JavaQuestions/Binary_Search_algorithm.java
@@ -2,8 +2,8 @@ package JavaQuestions;
 
 public class Binary_Search_algorithm {
     public static void main(String[] args) {
-        // int e[]={2,4,56,67,79,80}; // shorted array
-        int e[]={80,79,67,56,4,2};  // shorted array
+        int e[]={2,4,56,67,79,80}; // shorted array
+        // int e[]={80,79,67,56,4,2};  // shorted array
         int start=0;
         int end=e.length-1;
         int mid=(start+end)/2;
