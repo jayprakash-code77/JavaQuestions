5f7747dd2dd61cfa3f6d660427e1fbb0a2687aa8
Jaypraksh Maurya <jayprakashmcode7@gmail.com>
2024-06-01 12:30:01 +0530

    This commit is created from Intelij IDE

---
+++ practice.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class practice {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int num = s.nextInt();
+        System.out.println(num+" This is a input.");
+    }
+}
